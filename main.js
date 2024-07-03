a5a90d77cc4fa1a24e60776425a52848e2b70482
Sakib_Dalal <sakibdalal73@gmail.com>
2024-07-03 23:46:43 +0530

    commit 04

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+// this is comment
+/*
+this is multiline comment 
+*/
+var i = undefined;
+console.log(i);
+var i = 90;
+console.log(i);
+var i = true;
+console.log(i);
